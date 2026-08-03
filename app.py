1284c2b27825aa60b221236410582191bf6d799b
Vishnukumar-2003 <vk2346415@gmail.com>
2026-08-03 20:31:35 +0530

    feat(k8s): add ConfigMap, PV, PVC, Service, Deployment and health endpoint

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
+APP_NAME = os.getenv("APP_NAME", "Hello DevOps 🚀")
+ENVIRONMENT = os.getenv("ENVIRONMENT", "Production")
+
 @app.route("/")
 def home():
-    return "<h1>Hello DevOps 🚀</h1><h2>Flask + Docker + Jenkins</h2>"
+    return f"""
+    <h1>{APP_NAME}</h1>
+    <h2>Environment: {ENVIRONMENT}</h2>
+    <h3>Flask + Docker + Kubernetes + ConfigMap</h3>
+    """
+
+@app.route("/health")
+def health():
+    return "OK", 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
